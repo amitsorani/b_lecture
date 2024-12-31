b7cbd793e18e92fa2d7408aaa77131c70bcec5ec
amitsorani1204 <mtsorani@gmail.com>
2024-12-31 11:00:24 +0530

    image automatically change in spacific time

--- 3121.aspx
+++ 3121.aspx
@@ -1,16 +1,19 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="3121.aspx.cs" Inherits="demo1._3121" %>
 
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Image Slider</title>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-            <asp:AdRotator ID="AdRotator1" runat="server" AdvertisementFile="~/ad.xml" />
-        </div>
+        <asp:ScriptManager runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel runat="server">
+            <ContentTemplate>
+                <asp:Image ID="Image1" runat="server" Width="500px" Height="300px" />
+                <asp:Timer ID="Timer1" runat="server" Interval="2000" OnTick="Timer1_Tick"></asp:Timer>
+            </ContentTemplate>
+        </asp:UpdatePanel>
     </form>
 </body>
 </html>
--- 3121.aspx.cs
+++ 3121.aspx.cs
@@ -1,17 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace demo1
 {
     public partial class _3121 : System.Web.UI.Page
     {
+        private static List<string> imageUrls = new List<string>
+        {
+            "1.jpeg",
+            "2.jpeg",
+            "3.png"
+        };
+
+        private static int currentIndex = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                // Initialize the image on first load
+                DisplayImage();
+            }
+        }
 
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            // Change the image every 2 seconds
+            currentIndex = (currentIndex + 1) % imageUrls.Count;
+            DisplayImage();
+        }
+
+        private void DisplayImage()
+        {
+            // Set the image URL to the Image control
+            Image1.ImageUrl = imageUrls[currentIndex];
         }
     }
 }
